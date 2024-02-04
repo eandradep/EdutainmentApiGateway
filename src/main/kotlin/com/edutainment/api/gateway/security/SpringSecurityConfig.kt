abbbe2c44cb129ac76cb6cab21e3309ffdf73f9f
eandradep <edison.andrade@weconnect.com.ec>
2024-02-03 21:35:02 -0500

    :construction: Add new security configuration and authentication filter
    This commit includes the addition of crucial security configurations for the Edutainment API and a JWT authentication filter. The configurations specify roles and permissions for various API endpoints.

--- src/main/kotlin/com/edutainment/api/gateway/security/SpringSecurityConfig.kt
+++ src/main/kotlin/com/edutainment/api/gateway/security/SpringSecurityConfig.kt
@@ -27,29 +27,90 @@ class SpringSecurityConfig {
             .pathMatchers(
                 HttpMethod.GET,
                 "/edutainment/person/userProfileController/findPersonByIdentification/{personIdentification}",
-                "/edutainment/person/userProfileController/findStudents"
+                "/edutainment/person/userProfileController/findStudents",
+                "/edutainment/repositorie/api/crud/tags/{id}",
+            ).hasRole("ADMIN")
+            .pathMatchers(
+                HttpMethod.POST,
+                "/edutainment/repositorie/api/crud/tags",
+            ).hasRole("ADMIN")
+            .pathMatchers(
+                HttpMethod.DELETE,
+                "/edutainment/repositorie/api/crud/tags/**",
+            ).hasRole("ADMIN")
+            .pathMatchers(
+                HttpMethod.PATCH,
+                "/edutainment/repositorie/api/crud/tags/**",
             ).hasRole("ADMIN")
 
-//            USER LOGED ROLES
-
+//            COMBINATED ROLES
             .pathMatchers(
                 "/edutainment/dialogues/sequenceController/**",
                 "/edutainment/person/userProfileController/findPersonByID/{personID}",
                 "/edutainment/person/userProfileController/updatePerson/{personID}",
-                "/edutainment/person/userProfileController/createPerson"
+                "/edutainment/person/userProfileController/findPersonByUserID/{userId}",
+//
+                "/edutainment/repositorie/api/crud/tags",
             ).hasAnyRole("ADMIN", "USER")
+
+            .pathMatchers(
+                HttpMethod.POST,
+                "/edutainment/repositorie/api/crud/resources",
+                "/edutainment/repositorie/api/crud/repositories",
+                "/edutainment/repositorie/api/crud/games",
+                "/edutainment/filesystem/api/data/delete/resource",
+                "/edutainment/filesystem/api/data/delete/repositorie",
+                "/edutainment/filesystem/api/data/upload",
+            ).hasAnyRole("ADMIN", "USER")
+            .pathMatchers(
+                HttpMethod.GET,
+                "/edutainment/repositorie/api/crud/resources/**",
+                "/edutainment/repositorie/api/crud/resources/slug/**",
+            ).hasAnyRole("ADMIN", "USER")
+            .pathMatchers(
+                HttpMethod.PATCH,
+                "/edutainment/repositorie/api/crud/resources/**",
+                "/edutainment/repositorie/api/crud/repositories/**",
+                "/edutainment/repositorie/api/crud/games/**",
+            ).hasAnyRole("ADMIN", "USER")
+            .pathMatchers(
+                HttpMethod.DELETE,
+                "/edutainment/repositorie/api/crud/repositories/**",
+                "/edutainment/repositorie/api/crud/games/**"
+            ).hasAnyRole("ADMIN", "USER")
+            .pathMatchers(
+                HttpMethod.PUT,
+                "/edutainment/filesystem/api/data/update/resource",
+                "/edutainment/filesystem/api/data/update-names",
+            ).hasAnyRole("ADMIN", "USER")
+
 
 //            OPEN SERVICES
 
             .pathMatchers(
                 "/edutainment/oauth/**",
-                "/edutainment/person/genderController/findAllGender").permitAll()
+                "/edutainment/person/genderController/findAllGender",
+                "/edutainment/person/userProfileController/createPerson",
+                "/edutainment/repositorie/api/crud/tags",
+                "/edutainment/repositorie/api/crud/tags/search/{name}",
+                "/edutainment/repositorie/api/crud/resources",
+                "/edutainment/repositorie/api/crud/resources/search",
+                "/edutainment/repositorie/api/crud/resources/kind",
+                "/edutainment/repositorie/api/crud/repositories/**",
+                "/edutainment/repositorie/api/crud/repositories/slug/**",
+                "/edutainment/repositorie/api/crud/games",
+                "/edutainment/repositorie/api/crud/games/**",
+                "/edutainment/repositorie/api/crud/games/slug/**",
+                "/edutainment/filesystem/api/data/download",
+            ).permitAll()
             .pathMatchers(HttpMethod.GET).access { _, exchange ->
                 Mono.just(
                     AuthorizationDecision(
                         exchange.exchange.request.uri.path.contains("webjars/swagger-ui")
                                 or
                                 exchange.exchange.request.uri.path.contains("v3/api-docs")
+                                or
+                                exchange.exchange.request.uri.path.contains("encrypt-internal-controller/encrypt-password")
                     )
                 )
             }
