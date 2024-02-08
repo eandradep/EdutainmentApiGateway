3fbf6763bea57226ddc58919b3b535ce503a51ec
eandradep <edison.andrade@weconnect.com.ec>
2024-02-07 21:28:18 -0500

    Refactor security roles and update server-gateway image
    Security roles for 'ADMIN', 'USER', and 'REPOSITORY' in the Spring Security configuration have been restructured for better organization and readability. The method names and comments have been updated, making the source code easier to understand and maintain. The server-gateway image was also updated in the deployment service YAML file to match the new jar file's location.

--- src/main/kotlin/com/edutainment/api/gateway/security/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/edutainment/api/gateway/security/JwtAuthenticationFilter.kt
@@ -12,92 +12,37 @@ import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
 
-/**
- * JwtAuthenticationFilter is a Spring WebFilter that performs JWT token authentication.
- */
 @Component
 class JwtAuthenticationFilter : WebFilter {
+    private val logger = LoggerFactory.getLogger(JwtAuthenticationFilter::class.java)
 
-    /**
-     * This variable represents a logger factory for the JwtAuthenticationFilter class.
-     * It is used to create instances of ILogger that log messages for a specified class.
-     * The LoggerFactory.getLogger() method takes a Class<T> parameter to specify the class for which the logger is created.
-     */
-    private val loggerFactory = LoggerFactory.getLogger(JwtAuthenticationFilter::class.java)
-
-    /**
-     * The `authenticationManager` property is an instance of the `ReactiveAuthenticationManager` interface.
-     * It is used for performing authentication of JWT tokens in the `JwtAuthenticationFilter` class.
-     *
-     * @see JwtAuthenticationFilter
-     * @see ReactiveAuthenticationManager
-     */
     @Autowired
     private lateinit var authenticationManager: ReactiveAuthenticationManager
 
-    /**
-     * Filters the server web exchange to handle authentication using a JWT token.
-     *
-     * @param exchange The server web exchange.
-     * @param chain The web filter chain.
-     * @return A Mono that represents the completion of the request processing.
-     */
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
-        loggerFactory.info(" ---------> Start request <--------- ")
-
-        return Mono.justOrEmpty<String>(this.validateFirstPart(exchange))
-            .filter { authHeader: String -> this.validateStartWith(authHeader) }
-            .switchIfEmpty(this.validateSwithc(chain, exchange))
-            .map { token: String ->
-                loggerFactory.info("Token identify!")
-                token.replace(
-                    "Bearer ",
-                    ""
-                )
-            }
-            .flatMap { token: String? ->
-                authenticationManager.authenticate(
-                    UsernamePasswordAuthenticationToken(null, token)
-                )
+        logger.info(" ---------> Start request <--------- ")
+        return Mono.justOrEmpty(this.validateFirstPart(exchange))
+            .filter(this::validateStartWith)
+            .switchIfEmpty(this.validateSwitch(chain, exchange))
+            .map { token -> token.replace("Bearer ", "")}
+            .flatMap { token: String ->
+                authenticationManager.authenticate(UsernamePasswordAuthenticationToken(null, token))
             }
             .flatMap { authentication ->
-                loggerFactory.info(" ----------> End request <---------- ")
-                chain.filter(
-                    exchange
-                ).contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication))
+                logger.info(" ----------> End request <---------- ")
+                chain.filter(exchange).contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication))
             }
     }
 
-    private fun validateSwithc(chain: WebFilterChain, exchange: ServerWebExchange): Mono<out String> {
-        loggerFactory.info("validateSwithc");
-        val x: Mono<out String> = chain.filter(exchange).then(Mono.empty());
-
-        loggerFactory.info("validateStartWith. {}", x)
-        return x;
+    private fun validateSwitch(chain: WebFilterChain, exchange: ServerWebExchange): Mono<String> {
+        return chain.filter(exchange).then(Mono.empty())
     }
 
-
-    private fun validateStartWith(authHeader: String): Boolean {
-        loggerFactory.info("validateStartWith");
-        val x = authHeader.startsWith("Bearer ")
-        loggerFactory.info("validateStartWith. {}", x)
-        return x
-    }
+    private fun validateStartWith(authHeader: String): Boolean = authHeader.startsWith("Bearer ")
 
     private fun validateFirstPart(exchange: ServerWebExchange): String? {
-        loggerFactory.info("data del loggesr");
-        loggerFactory.info("headers {}", exchange.request.headers);
-        exchange.request.headers.forEach { entry ->
-            loggerFactory.info("headers {}", entry.key)
-            if (entry.key.equals("Access-Control-Request-Headers")) {
-                entry.value.forEach { s: String? ->
-                    loggerFactory.info("Access-Control-Request-Headers {}", s)
-                }
-            }
-        }
-        ;
-        val x = exchange.request.headers.getFirst(HttpHeaders.AUTHORIZATION)
-        loggerFactory.info("data del loggesr. {}", x)
-        return x
+        val authHeader = exchange.request.headers.getFirst(HttpHeaders.AUTHORIZATION)
+        logger.info("Authorization token: {}", authHeader)
+        return authHeader
     }
 }
--- src/main/kotlin/com/edutainment/api/gateway/security/SpringSecurityConfig.kt
+++ src/main/kotlin/com/edutainment/api/gateway/security/SpringSecurityConfig.kt
@@ -39,11 +39,12 @@ class SpringSecurityConfig {
     @Bean
     fun configure(http: ServerHttpSecurity): SecurityWebFilterChain {
         return http.authorizeExchange()
-            .let { getAdminConfigurations(it) }
+//            .let { getAdminConfigurations(it) }
             .let { getAdminRepositoryConfigurations(it) }
 //            .let { getUserConfigurations(it) }
 //            .let { getAdminUserConfigurations(it) }
-//            .let { getNoAuthRequiredConfigurations(it) }
+
+            .let { getNoAuthRequiredConfigurations(it) }
 
 //            COMBINATED ROUTES ADMIN AND USER
 
@@ -61,8 +62,7 @@ class SpringSecurityConfig {
      * @param http The ServerHttpSecurity.AuthorizeExchangeSpec to configure the admin configurations.
      * @return The configured ServerHttpSecurity.AuthorizeExchangeSpec with admin configurations.
      */
-    private fun getAdminConfigurations
-                (http: ServerHttpSecurity.AuthorizeExchangeSpec): ServerHttpSecurity.AuthorizeExchangeSpec {
+    private fun getAdminConfigurations(http: ServerHttpSecurity.AuthorizeExchangeSpec): ServerHttpSecurity.AuthorizeExchangeSpec {
         return http
             .pathMatchers(
                 HttpMethod.POST,
@@ -90,6 +90,7 @@ class SpringSecurityConfig {
             .hasRole("ADMIN")
 
     }
+
     /**
      * Configures the admin repository configurations for the given ServerHttpSecurity.AuthorizeExchangeSpec instance.
      *
@@ -97,40 +98,40 @@ class SpringSecurityConfig {
      * @return The configured ServerHttpSecurity.AuthorizeExchangeSpec.
      */
     private fun getAdminRepositoryConfigurations(
-        http: ServerHttpSecurity.AuthorizeExchangeSpec): ServerHttpSecurity.AuthorizeExchangeSpec {
+        http: ServerHttpSecurity.AuthorizeExchangeSpec
+    ): ServerHttpSecurity.AuthorizeExchangeSpec {
         return http
             .pathMatchers(
                 HttpMethod.POST,
-                "/edutainment/repositorie/api/crud/resources",
-                "/edutainment/repositorie/api/crud/repositories",
-                "/edutainment/repositorie/api/crud/games",
+                "/edutainment/filesystem/api/data/upload",
                 "/edutainment/filesystem/api/data/delete/resource",
                 "/edutainment/filesystem/api/data/delete/repositorie",
-                "/edutainment/filesystem/api/data/upload",
-            ).hasAnyRole("ADMIN", "REPOSITORY")
-            .pathMatchers(
-                HttpMethod.GET,
-                "/edutainment/repositorie/api/crud/resources/**",
-                "/edutainment/repositorie/api/crud/resources/slug/**",
-            ).hasAnyRole("ADMIN", "REPOSITORY")
-            .pathMatchers(
-                HttpMethod.PATCH,
-                "/edutainment/repositorie/api/crud/resources/**",
-                "/edutainment/repositorie/api/crud/repositories/**",
-                "/edutainment/repositorie/api/crud/games/**",
-            ).hasAnyRole("ADMIN", "REPOSITORY")
-            .pathMatchers(
-                HttpMethod.DELETE,
-                "/edutainment/repositorie/api/crud/repositories/**",
-                "/edutainment/repositorie/api/crud/games/**"
+                "/edutainment/repositorie/api/crud/games/createGame",
+                "/edutainment/repositorie/api/crud/repositories/createRepositorie",
+                "/edutainment/repositorie/api/crud/resources/createResource",
+                "/edutainment/repositorie/api/crud/tags/createTag",
             ).hasAnyRole("ADMIN", "REPOSITORY")
             .pathMatchers(
                 HttpMethod.PUT,
                 "/edutainment/filesystem/api/data/update/resource",
                 "/edutainment/filesystem/api/data/update-names",
             ).hasAnyRole("ADMIN", "REPOSITORY")
-
+            .pathMatchers(
+                HttpMethod.PATCH,
+                "/edutainment/repositorie/api/crud/games/updateGameById/{id}",
+                "/edutainment/repositorie/api/crud/repositories/updateRepositorieById/{id}",
+                "/edutainment/repositorie/api/crud/resources/updateResourceById/{id}",
+                "/edutainment/repositorie/api/crud/tags/updateTagById/{id}",
+            ).hasAnyRole("ADMIN", "REPOSITORY")
+            .pathMatchers(
+                HttpMethod.DELETE,
+                "/edutainment/repositorie/api/crud/games/deleteGameById/{id}",
+                "/edutainment/repositorie/api/crud/repositories/deleteRepositorieById/{id}",
+                "/edutainment/repositorie/api/crud/resources/deleteResourceById/{id}",
+                "/edutainment/repositorie/api/crud/tags/deleteTagById/{id}",
+            ).hasAnyRole("ADMIN", "REPOSITORY")
     }
+
     /**
      * Retrieves the configurations for admin users in the HTTP security.
      *
@@ -138,7 +139,8 @@ class SpringSecurityConfig {
      * @return The modified ServerHttpSecurity.AuthorizeExchangeSpec with the admin user configurations applied.
      */
     private fun getAdminUserConfigurations(
-        http: ServerHttpSecurity.AuthorizeExchangeSpec): ServerHttpSecurity.AuthorizeExchangeSpec {
+        http: ServerHttpSecurity.AuthorizeExchangeSpec
+    ): ServerHttpSecurity.AuthorizeExchangeSpec {
         return http
             .pathMatchers(
                 HttpMethod.GET,
@@ -155,7 +157,8 @@ class SpringSecurityConfig {
     }
 
     private fun getUserConfigurations(
-        http: ServerHttpSecurity.AuthorizeExchangeSpec): ServerHttpSecurity.AuthorizeExchangeSpec {
+        http: ServerHttpSecurity.AuthorizeExchangeSpec
+    ): ServerHttpSecurity.AuthorizeExchangeSpec {
         return http
             .pathMatchers(
                 HttpMethod.POST,
@@ -164,6 +167,7 @@ class SpringSecurityConfig {
             .hasAnyRole("ADMIN", "USER")
 
     }
+
     /**
      * Retrieves the configurations for endpoints that do not require authentication.
      *
@@ -171,24 +175,38 @@ class SpringSecurityConfig {
      * @return The configured ServerHttpSecurity.AuthorizeExchangeSpec.
      */
     private fun getNoAuthRequiredConfigurations(
-        http: ServerHttpSecurity.AuthorizeExchangeSpec): ServerHttpSecurity.AuthorizeExchangeSpec {
+        http: ServerHttpSecurity.AuthorizeExchangeSpec
+    ): ServerHttpSecurity.AuthorizeExchangeSpec {
         return http
+            .pathMatchers(HttpMethod.OPTIONS,
+                "/**")
+            .permitAll()
             .pathMatchers(
                 "/edutainment/oauth/**",
                 "/edutainment/person/genderController/findAllGender",
                 "/edutainment/person/userProfileController/createPerson",
                 "/edutainment/student/student-controller/createStudent",
-                "/edutainment/repositorie/api/crud/tags",
-                "/edutainment/repositorie/api/crud/tags/search/**",
-                "/edutainment/repositorie/api/crud/resources",
-                "/edutainment/repositorie/api/crud/resources/search",
-                "/edutainment/repositorie/api/crud/resources/kind",
-                "/edutainment/repositorie/api/crud/repositories/**",
-                "/edutainment/repositorie/api/crud/repositories/slug/**",
-                "/edutainment/repositorie/api/crud/games",
-                "/edutainment/repositorie/api/crud/games/**",
-                "/edutainment/repositorie/api/crud/games/slug/**",
-                "/edutainment/filesystem/api/data/download",
+
+                "/edutainment/repositorie/api/crud/games/findGameById/{id}",
+                "/edutainment/repositorie/api/crud/games/findGameBySlug/{slug}",
+                "/edutainment/repositorie/api/crud/games/findAllGamesByPagination",
+                "/edutainment/repositorie/api/crud/repositories/findRepositorieById/{id}",
+                "/edutainment/repositorie/api/crud/repositories/findRepositorieBySlug/{slug}",
+                "/edutainment/repositorie/api/crud/repositories/findAllRepositoriesByPagination",
+                "/edutainment/repositorie/api/crud/resources/findResourceById/{id}",
+                "/edutainment/repositorie/api/crud/resources/findResourceBySlug/{slug}",
+                "/edutainment/repositorie/api/crud/resources/findAllResourcesByPagination",
+                "/edutainment/repositorie/api/crud/resources/findAllResourcesPaginatedByName/search",
+                "/edutainment/repositorie/api/crud/resources/findAllResourcesPaginatedByKindAndTags/kind",
+                "/edutainment/repositorie/api/crud/resources/findAllResourcesPaginatedByKindAndTags/kind",
+
+                "/edutainment/repositorie/api/crud/tags/findTagById/{id}",
+                "/edutainment/repositorie/api/crud/tags/findAllTagsByPagination",
+                "/edutainment/repositorie/api/crud/tags/findAllTagsPaginatedByName/{name}",
+
+                "/edutainment/filesystem/api/data/download-resource",
+                "/edutainment/media-references/**",
+                "/edutainment/resources/**",
             )
             .permitAll()
             .pathMatchers(HttpMethod.GET).access { _, exchange ->
@@ -197,8 +215,6 @@ class SpringSecurityConfig {
                         exchange.exchange.request.uri.path.contains("webjars/swagger-ui")
                                 or
                                 exchange.exchange.request.uri.path.contains("v3/api-docs")
-                                or
-                                exchange.exchange.request.uri.path.contains("encrypt-internal-controller/encrypt-password")
                     )
                 )
             }
